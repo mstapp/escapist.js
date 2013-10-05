ae975ae986eac45eacf1a526b4cfc17718303ac8
Mike Stapp <mstapp@tiggertail.com>
2013-10-04 22:44:00 -0400

    Drop '$' prefix for var names
    This unusual style made the code really hard to read, especially
    in the age of jquery.

--- src/escapist.js
+++ src/escapist.js
@@ -6,67 +6,67 @@
     window.secure = window.escapist;
 
 
-    escapist.html = function($str) {
-        if ($str == null || $str.length == 0) {
-            $str = '';
+    escapist.html = function(str) {
+        if (str == null || str.length == 0) {
+            str = '';
         }
 
-        var $out = '';
-        var $len = $str.length;
+        var out = '';
+        var len = str.length;
 
         // Allow: a-z A-Z 0-9 SPACE , .
         // Allow (dec): 97-122 65-90 48-57 32 44 46
 
-        for (var $cnt = 0; $cnt < $len; $cnt++) {
+        for (var cnt = 0; cnt < len; cnt++) {
 
-            var $c = $str.charCodeAt($cnt);
+            var c = str.charCodeAt(cnt);
 
-            if ( ($c >= 97 && $c <= 122) ||
-                ($c >= 65 && $c <= 90 ) ||
-                ($c >= 48 && $c <= 57 ) ||
-                $c == 32 || $c == 44 || $c == 46 ) {
+            if ( (c >= 97 && c <= 122) ||
+                (c >= 65 && c <= 90 ) ||
+                (c >= 48 && c <= 57 ) ||
+                c == 32 || c == 44 || c == 46 ) {
 
-                $out += $str.charAt($cnt);
+                out += str.charAt(cnt);
             }
             else {
-                $out += '&#' + $c + ';';
+                out += '&#' + c + ';';
             }
         }
 
-        return $out;
+        return out;
     };
 
     // legacy Reform API
     escapist.HtmlEncode = escapist.html;
 
 
-    escapist.attr = function($str) {
-        if ($str == null || $str.length == 0) {
-            $str = '';
+    escapist.attr = function(str) {
+        if (str == null || str.length == 0) {
+            str = '';
         }
 
-        var $out = '';
-        var $len = $str.length;
+        var out = '';
+        var len = str.length;
 
         // Allow: a-z A-Z 0-9
         // Allow (dec): 97-122 65-90 48-57
 
-        for (var $cnt = 0; $cnt < $len; $cnt++) {
+        for (var cnt = 0; cnt < len; cnt++) {
 
-            var $c = $str.charCodeAt($cnt);
+            var c = str.charCodeAt(cnt);
 
-            if ( ($c >= 97 && $c <= 122) ||
-                ($c >= 65 && $c <= 90 ) ||
-                ($c >= 48 && $c <= 57 ) ) {
+            if ( (c >= 97 && c <= 122) ||
+                (c >= 65 && c <= 90 ) ||
+                (c >= 48 && c <= 57 ) ) {
 
-                $out += $str.charAt($cnt);
+                out += str.charAt(cnt);
             }
             else {
-                $out += '&#' + $c + ';';
+                out += '&#' + c + ';';
             }
         }
 
-        return $out;
+        return out;
     };
 
     // legacy Reform API
