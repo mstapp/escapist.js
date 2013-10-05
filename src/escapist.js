df90944357261c39ce053cc677472a852cf9da70
Mike Stapp <mstapp@tiggertail.com>
2013-10-04 22:06:34 -0400

    Move all code inside IIFE, change indent level
    No code changes except change of indentation level, and wrapping
    the code in an IIFE to provide local scope.

--- src/escapist.js
+++ src/escapist.js
@@ -1,131 +1,134 @@
 
-// global namespace 'escapist', plus alias 'secure'
-escapist = escapist || {};
-secure = escapist;
+(function(){
+
+    // global namespace 'escapist', plus alias 'secure'
+    escapist = escapist || {};
+    secure = escapist;
 
 
-escapist.HtmlEncode = function($str, $default) {
-    if($str == null || $str.length == 0)
-    {
-        $str = ($default == null ? '' : $default);
-    }
-
-    $out = '';
-    $len = $str.length;
-
-    // Allow: a-z A-Z 0-9 SPACE , .
-    // Allow (dec): 97-122 65-90 48-57 32 44 46
-
-    for($cnt = 0; $cnt < $len; $cnt++)
-    {
-        $c = $str.charCodeAt($cnt);
-        if( ($c >= 97 && $c <= 122) ||
-            ($c >= 65 && $c <= 90 ) ||
-            ($c >= 48 && $c <= 57 ) ||
-            $c == 32 || $c == 44 || $c == 46 )
-        {
-            $out += $str.charAt($cnt);
-        }
-        else
-        {
-            $out += '&#' + $c + ';';
-        }
-    }
-
-    return $out;
-};
-
-escapist.HtmlAttributeEncode = function($str, $default) {
-    if($str == null || $str.length == 0)
-    {
-        $str = ($default == null ? '' : $default);
-    }
-
-    $out = '';
-    $len = $str.length;
-
-    // Allow: a-z A-Z 0-9
-    // Allow (dec): 97-122 65-90 48-57
-
-    for($cnt = 0; $cnt < $len; $cnt++)
-    {
-        $c = $str.charCodeAt($cnt);
-        if( ($c >= 97 && $c <= 122) ||
-            ($c >= 65 && $c <= 90 ) ||
-            ($c >= 48 && $c <= 57 ) )
-        {
-            $out += $str.charAt($cnt);
-        }
-        else
-        {
-            $out += '&#' + $c + ';';
-        }
-    }
-
-    return $out;
-};
-
-var hD="0123456789ABCDEF";
-function d2h(d) {
-    var h = hD.substr(d&15,1);
-    while(d>15)
-    {
-        d>>=4;
-        h=hD.substr(d&15,1)+h;
-    }
-
-    return h;
-};
-
-escapist.JsString = function($str, $default) {
-    if($str == null || $str.length == 0)
-    {
-        $str = ($default == null ? '' : $default);
-
+    escapist.HtmlEncode = function($str, $default) {
         if($str == null || $str.length == 0)
         {
-            return '\'\'';
+            $str = ($default == null ? '' : $default);
         }
-    }
 
-    $out = '\'';
-    $len = $str.length;
+        $out = '';
+        $len = $str.length;
 
-    // Allow: a-z A-Z 0-9 SPACE , .
-    // Allow (dec): 97-122 65-90 48-57 32 44 46
+        // Allow: a-z A-Z 0-9 SPACE , .
+        // Allow (dec): 97-122 65-90 48-57 32 44 46
 
-    for($cnt = 0; $cnt < $len; $cnt++)
-    {
-        $c = $str.charCodeAt($cnt);
-        if( ($c >= 97 && $c <= 122) ||
-            ($c >= 65 && $c <= 90 ) ||
-            ($c >= 48 && $c <= 57 ) ||
-            $c == 32 || $c == 44 || $c == 46 )
+        for($cnt = 0; $cnt < $len; $cnt++)
         {
-            $out += $str.charAt($cnt);
-        }
-        else if( $c <= 127 )
-        {
-            $hex = d2h($c);
-            if( $hex.length < 2 )
+            $c = $str.charCodeAt($cnt);
+            if( ($c >= 97 && $c <= 122) ||
+                ($c >= 65 && $c <= 90 ) ||
+                ($c >= 48 && $c <= 57 ) ||
+                $c == 32 || $c == 44 || $c == 46 )
             {
-                '0' + $hex;
+                $out += $str.charAt($cnt);
             }
-
-            $out += '\\x' + $hex;
-        }
-        else
-        {
-            $hex = d2h($c);
-            while( $hex.length < 4 )
+            else
             {
-                $hex = '0' + $hex;
+                $out += '&#' + $c + ';';
             }
-
-            $out += '\\u' + $hex;
         }
-    }
 
-    return $out + '\'';
-};
+        return $out;
+    };
 
+    escapist.HtmlAttributeEncode = function($str, $default) {
+        if($str == null || $str.length == 0)
+        {
+            $str = ($default == null ? '' : $default);
+        }
+
+        $out = '';
+        $len = $str.length;
+
+        // Allow: a-z A-Z 0-9
+        // Allow (dec): 97-122 65-90 48-57
+
+        for($cnt = 0; $cnt < $len; $cnt++)
+        {
+            $c = $str.charCodeAt($cnt);
+            if( ($c >= 97 && $c <= 122) ||
+                ($c >= 65 && $c <= 90 ) ||
+                ($c >= 48 && $c <= 57 ) )
+            {
+                $out += $str.charAt($cnt);
+            }
+            else
+            {
+                $out += '&#' + $c + ';';
+            }
+        }
+
+        return $out;
+    };
+
+    var hD="0123456789ABCDEF";
+    function d2h(d) {
+        var h = hD.substr(d&15,1);
+        while(d>15)
+        {
+            d>>=4;
+            h=hD.substr(d&15,1)+h;
+        }
+
+        return h;
+    };
+
+    escapist.JsString = function($str, $default) {
+        if($str == null || $str.length == 0)
+        {
+            $str = ($default == null ? '' : $default);
+
+            if($str == null || $str.length == 0)
+            {
+                return '\'\'';
+            }
+        }
+
+        $out = '\'';
+        $len = $str.length;
+
+        // Allow: a-z A-Z 0-9 SPACE , .
+        // Allow (dec): 97-122 65-90 48-57 32 44 46
+
+        for($cnt = 0; $cnt < $len; $cnt++)
+        {
+            $c = $str.charCodeAt($cnt);
+            if( ($c >= 97 && $c <= 122) ||
+                ($c >= 65 && $c <= 90 ) ||
+                ($c >= 48 && $c <= 57 ) ||
+                $c == 32 || $c == 44 || $c == 46 )
+            {
+                $out += $str.charAt($cnt);
+            }
+            else if( $c <= 127 )
+            {
+                $hex = d2h($c);
+                if( $hex.length < 2 )
+                {
+                    '0' + $hex;
+                }
+
+                $out += '\\x' + $hex;
+            }
+            else
+            {
+                $hex = d2h($c);
+                while( $hex.length < 4 )
+                {
+                    $hex = '0' + $hex;
+                }
+
+                $out += '\\u' + $hex;
+            }
+        }
+
+        return $out + '\'';
+    };
+
+}).call(this);
