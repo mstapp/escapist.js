ae5dcee06219b98e60db8e03b13643cd1cb91edd
Mike Stapp <mstapp@tiggertail.com>
2013-10-04 22:28:18 -0400

    Minor code style changes: opening-brace placement
    Changed code style to be consistent with the rest of my code: pulled
    opening braces up to end of line that starts the expression, and
    added space between 'if'/'for' and opening parens.
    
    These are style changes only; no functional change.

--- src/escapist.js
+++ src/escapist.js
@@ -1,5 +1,5 @@
 
-(function(window){
+(function(window) {
 
     // global namespace 'escapist', plus alias 'secure'
     var escapist = window.escapist = {};
@@ -7,8 +7,7 @@
 
 
     escapist.html = function($str, $default) {
-        if($str == null || $str.length == 0)
-        {
+        if ($str == null || $str.length == 0) {
             $str = ($default == null ? '' : $default);
         }
 
@@ -18,18 +17,15 @@
         // Allow: a-z A-Z 0-9 SPACE , .
         // Allow (dec): 97-122 65-90 48-57 32 44 46
 
-        for($cnt = 0; $cnt < $len; $cnt++)
-        {
+        for ($cnt = 0; $cnt < $len; $cnt++) {
             $c = $str.charCodeAt($cnt);
-            if( ($c >= 97 && $c <= 122) ||
+            if ( ($c >= 97 && $c <= 122) ||
                 ($c >= 65 && $c <= 90 ) ||
                 ($c >= 48 && $c <= 57 ) ||
-                $c == 32 || $c == 44 || $c == 46 )
-            {
+                $c == 32 || $c == 44 || $c == 46 ) {
                 $out += $str.charAt($cnt);
             }
-            else
-            {
+            else {
                 $out += '&#' + $c + ';';
             }
         }
@@ -42,8 +38,7 @@
 
 
     escapist.attr = function($str, $default) {
-        if($str == null || $str.length == 0)
-        {
+        if ($str == null || $str.length == 0) {
             $str = ($default == null ? '' : $default);
         }
 
@@ -53,17 +48,15 @@
         // Allow: a-z A-Z 0-9
         // Allow (dec): 97-122 65-90 48-57
 
-        for($cnt = 0; $cnt < $len; $cnt++)
-        {
+        for ($cnt = 0; $cnt < $len; $cnt++) {
             $c = $str.charCodeAt($cnt);
-            if( ($c >= 97 && $c <= 122) ||
+            if ( ($c >= 97 && $c <= 122) ||
                 ($c >= 65 && $c <= 90 ) ||
-                ($c >= 48 && $c <= 57 ) )
-            {
+                ($c >= 48 && $c <= 57 ) ) {
+
                 $out += $str.charAt($cnt);
             }
-            else
-            {
+            else {
                 $out += '&#' + $c + ';';
             }
         }
