c04dcd0cd9655a9cd6a1f73bd9c40907c69e365d
Mike Stapp <mstapp@tiggertail.com>
2013-10-04 22:33:59 -0400

    Bug fix: use 'var' for local vars
    Original code was accidentally creating global vars instead of
    locals inside of functions.

--- src/escapist.js
+++ src/escapist.js
@@ -11,18 +11,21 @@
             $str = ($default == null ? '' : $default);
         }
 
-        $out = '';
-        $len = $str.length;
+        var $out = '';
+        var $len = $str.length;
 
         // Allow: a-z A-Z 0-9 SPACE , .
         // Allow (dec): 97-122 65-90 48-57 32 44 46
 
-        for ($cnt = 0; $cnt < $len; $cnt++) {
-            $c = $str.charCodeAt($cnt);
+        for (var $cnt = 0; $cnt < $len; $cnt++) {
+
+            var $c = $str.charCodeAt($cnt);
+
             if ( ($c >= 97 && $c <= 122) ||
                 ($c >= 65 && $c <= 90 ) ||
                 ($c >= 48 && $c <= 57 ) ||
                 $c == 32 || $c == 44 || $c == 46 ) {
+
                 $out += $str.charAt($cnt);
             }
             else {
@@ -42,14 +45,16 @@
             $str = ($default == null ? '' : $default);
         }
 
-        $out = '';
-        $len = $str.length;
+        var $out = '';
+        var $len = $str.length;
 
         // Allow: a-z A-Z 0-9
         // Allow (dec): 97-122 65-90 48-57
 
-        for ($cnt = 0; $cnt < $len; $cnt++) {
-            $c = $str.charCodeAt($cnt);
+        for (var $cnt = 0; $cnt < $len; $cnt++) {
+
+            var $c = $str.charCodeAt($cnt);
+
             if ( ($c >= 97 && $c <= 122) ||
                 ($c >= 65 && $c <= 90 ) ||
                 ($c >= 48 && $c <= 57 ) ) {
